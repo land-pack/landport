0a9fe058e25e5e0d54a286a299c000e5399dbf50
landpack <ada.frank.ak@gmail.com>
2017-02-07 14:36:11 +0800

    [test] server should no call on_close directory

--- example/tornado/websocket.py
+++ example/tornado/websocket.py
@@ -2,19 +2,39 @@ from tornado import websocket
 from tornado import ioloop
 from tornado import web
 
+clients = {}
+
+
+
 class WebsocketHandler(websocket.WebSocketHandler):
     def check_origin(self, origin):
+        print '>> check_origin'
         return True
 
     def open(self):
-        pass
+        print '>> open'
+        clients[id(self)] = self
 
     def on_message(self, message):
+        print '>> on_message'
         self.write_message(message)
+        if 'shutdown' in message:
+            io_loop = ioloop.IOLoop.instance()
+            ioloop.PeriodicCallback(self.kick_off, 5000).start()
+            self.write_message('shutdown handler after 5s')
 
     def on_close(self):
-        NodeManager.unregister(self)
+        print '>> on_close'
+        del clients[id(self)]
 
+    def close(self):
+        print '>> close'
+        super(WebsocketHandler, self).close()
+
+    def kick_off(self):
+        id_handler = id(self)
+        if id_handler in clients:
+            clients[id_handler].close()
 
 
 if __name__ == '__main__':
