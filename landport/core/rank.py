7472f7e6ef270f5624940bccc01e32cb1f73a6e1
landpack <ada.frank.ak@gmail.com>
2017-02-21 22:43:50 +0800

    [update] rank

--- landport/core/rank.py
+++ landport/core/rank.py
@@ -1,27 +1,29 @@
+def cmp_cir(a, b):
+	if a == b:
+		pass
+
+
+
 class Ranklist(object):
 
 
-	def __init__(self, name, age, score):
+	def __init__(self, name, *args,**kwargs):
 		self.name = name
-		self.age = age
-		self.score = score
+		self.__dict__ = kwargs
+		self.args = args
+
 
 	def __lt__(self, other):	
-		if self.name == other.name:
-			if self.age == other.age:
-				return self.score < other.score
-			return self.age < other.age
-		return self.name < other.name
+		for k in self.args:
+			other_value = getattr(other, k)
+			v = getattr(self, k)
+			if v == other_value:continue
+			return v < other_value
+		return True
 
-	def __gt__(self, other):
-		if self.name == other.name:
-			if self.age == other.age:
-				return self.score > other.score 
-			return self.age > other.age 
-		return self.name > other.name
 
 	def __str__(self):
-		return '({}-{}-{})'.format(self.name, self.age, self.score)
+		return '(rank_list:{})'.format(self.name)
 
 	def __repr__(self):
 		return self.__str__()
@@ -31,13 +33,23 @@ class RankManager(object):
 		pass
 
 if __name__ == '__main__':
-	p1 = Ranklist(1, 2, 2)
-	p2 = Ranklist(1, 2, 3)
-	p3 = Ranklist(1, 2, 1)
-	p4 = Ranklist(1, 4, 3)
-	p5 = Ranklist(4, 2, 3)
-	p6 = Ranklist(4, 2, 3)
-	p = [p1, p2, p3, p4, p5, p6]
-	print(p)
-	pp = sorted(p)
-	print(pp)
+	p1 = Ranklist('jack' ,'chinese', 'math', chinese=95, math=88)
+	p2 = Ranklist('lisa' ,'chinese', 'math', chinese=96, math=81)
+	print p1 > p2
+	p1 = Ranklist('jack' ,'math', 'chinese', chinese=95, math=88)
+	p2 = Ranklist('lisa' ,'math', 'chinese', chinese=96, math=81)
+	print p1 > p2
+	d1 = {"a":23,"b":12,"c":88,"d":11}
+	d2 = {"a":3,"b":22,"c":88,"d":11}
+	p1 = Ranklist('ww', 'a','c','b', **d1)
+	p2 = Ranklist('vv', 'a','c','b', **d2)
+	print p1 > p2 
+	# p2 = Ranklist(1, 2, 3)
+	# p3 = Ranklist(1, 2, 1)
+	# p4 = Ranklist(1, 4, 3)
+	# p5 = Ranklist(4, 2, 3)
+	# p6 = Ranklist(4, 2, 3)
+	# p = [p1, p2, p3, p4, p5, p6]
+	# print(p)
+	# pp = sorted(p)
+	# print(pp)
